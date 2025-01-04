fc8489e87a6c2f44c120401b0b933841094470dd
Minh <mtran1712@gmail.com>
2025-01-03 19:03:13 -0800

    feat: add mui, configure config to nextjs, added simple image list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,96 @@
-import Image from "next/image";
+"use client";
+
+import * as React from "react";
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import styled from "@emotion/styled";
+import { useMediaQuery } from "@mui/material";
 
 export default function Home() {
+  const itemData = [
+    {
+      img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
+      title: "Bed",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
+      title: "Books",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
+      title: "Sink",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
+      title: "Kitchen",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
+      title: "Blinds",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
+      title: "Chairs",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
+      title: "Laptop",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
+      title: "Doors",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
+      title: "Coffee",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
+      title: "Storage",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
+      title: "Candle",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+      title: "Coffee table",
+    },
+  ];
+
+  const StyledContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    overflow: hidden;
+    max-width: 1440px;
+    margin: 0 auto;
+
+    @media (max-width: 600px) {
+      padding: 0 1rem;
+    }
+  `;
+
+  const matchBreakpoints = useMediaQuery((theme) =>
+    theme.breakpoints.down("sm")
+  );
+
   return (
-    <div>
-      <main>
-        hello world
-      </main>
-      <footer>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <StyledContainer>
+      <Box sx={{ width: "100%", height: "100%" }}>
+        <ImageList variant="masonry" cols={matchBreakpoints ? 1 : 3} gap={15}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Box>
+    </StyledContainer>
   );
 }
